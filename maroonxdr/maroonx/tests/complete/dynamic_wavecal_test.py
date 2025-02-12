5784ea0cacc86fcbc2ad82d97cb4aa06a9b9abf0
mchalela <tinchochalela@gmail.com>
2025-02-12 17:33:31 -0400

    observer session for non destructive checks

--- maroonxdr/maroonx/tests/complete/dynamic_wavecal_test.py
+++ maroonxdr/maroonx/tests/complete/dynamic_wavecal_test.py
@@ -1,24 +1,20 @@
-
 import glob
-from recipe_system.reduction.coreReduce import Reduce
 import sys
-import astrodata
-
 from pathlib import Path
+
+from recipe_system.reduction.coreReduce import Reduce
+
 parent_dir = Path(__file__).parents[4]
 sys.path.append(str(parent_dir))
-import maroonx_instruments
 
-<<<<<<< HEAD
-all_files = glob.glob('/Users/rohan/Desktop/DRAGONS-X/MAROONXDR/20220725T031211Z_DEEEE_r_0003_dynamic_wavecal.fits')
-=======
-all_files = glob.glob('/Users/rohan/Documents/20240411T153824Z_DEEEE_b_0030.fits')
->>>>>>> 9289dd70091d33872455b5dab3b5248b5a783cbe
 
+all_files = glob.glob(
+    '/Users/rohan/Desktop/DRAGONS-X/MAROONXDR/20220725T031211Z_DEEEE_r_0003_dynamic_wavecal.fits'
+)
 all_files.sort()
 
 # Run reduce on all selected files
 myreduce = Reduce()
 myreduce.files.extend(all_files)
-myreduce.drpkg= 'maroonxdr'
+myreduce.drpkg = 'maroonxdr'
 myreduce.runr()
--- noxfile.py
+++ noxfile.py
@@ -5,32 +5,34 @@ from pathlib import Path
 
 import nox
 
-nox.options.sessions = ["lint", "ruff_format"]
+# Commenting out formating sessions, waiting to have better tests
+# nox.options.sessions = ["lint", "ruff_format"]
+nox.options.sessions = ['observer']
 
 
 def get_dependencies(session: nox.Session) -> list[str]:
     """Get dependencies from `pyproject.toml` using Poetry."""
-    lockfile_path = Path("poetry.lock")
+    lockfile_path = Path('poetry.lock')
 
     if not lockfile_path.exists():
-        session.run("poetry", "lock", external=True)
+        session.run('poetry', 'lock', external=True)
 
     result = session.run(
-        "poetry",
-        "show",
-        "--top-level",
+        'poetry',
+        'show',
+        '--top-level',
         silent=True,
         external=True,
     )
 
-    result = result.replace("(!)", "   ")
+    result = result.replace('(!)', '   ')
 
     dependencies = []
     for line in result.splitlines():
-        if match := re.match(r"^\s*(\S+)\s+([\.0-9vV]+)\s*.*$", line):
+        if match := re.match(r'^\s*(\S+)\s+([\.0-9vV]+)\s*.*$', line):
             name = match.group(1)
             version = match.group(2)
-            dependencies.append(f"{name}=={version}")
+            dependencies.append(f'{name}=={version}')
 
     return dependencies
 
@@ -38,64 +40,76 @@ def get_dependencies(session: nox.Session) -> list[str]:
 @nox.session
 def create_venv(session: nox.Session):
     """Create a new virtual environment for development."""
-    session.install("poetry", "poetry-plugin-export", "virtualenv")
+    session.install('poetry', 'poetry-plugin-export', 'virtualenv')
     dependencies = get_dependencies(session)
 
     session.run(
-        "virtualenv",
-        "venv",
-        "--prompt=(MAROONXDR)",
+        'virtualenv',
+        'venv',
+        '--prompt=(MAROONXDR)',
     )
 
     # Install dependencies
-    session.run("venv/bin/pip", "install", *dependencies, external=True)
-    session.log("Dependencies installed. Listing them below:")
-    session.run("venv/bin/pip", "freeze", external=True)
+    session.run('venv/bin/pip', 'install', *dependencies, external=True)
+    session.log('Dependencies installed. Listing them below:')
+    session.run('venv/bin/pip', 'freeze', external=True)
 
     # Initialize pre-commit hooks
-    session.notify("initialize_commit_hooks")
+    session.notify('initialize_commit_hooks')
 
 
-@nox.session(venv_backend="conda")
+@nox.session(venv_backend='conda')
 def create_conda_env(session: nox.Session):
     """Create a new conda environment for development."""
-    session.install("poetry", "poetry-plugin-export")
+    session.install('poetry', 'poetry-plugin-export')
 
     # Get the dependencies directly using get_dependencies
     dependencies = get_dependencies(session)
 
     # Create the Conda environment
     session.run(
-        "conda",
-        "create",
-        "--force",
-        "--name=mx_dragons",
-        "--yes",
-        "python=3.9",  # You can specify the version of Python if needed
+        'conda',
+        'create',
+        '--force',
+        '--name=mx_dragons',
+        '--yes',
+        'python=3.12',
     )
 
     # Install dependencies using conda for each dependency
     for dep in dependencies:
         session.run(
-            "conda", "install", "--name=mx_dragons", "--yes", dep, external=True
+            'conda', 'install', '--name=mx_dragons', '--yes', dep, external=True
         )
 
     # Initialize pre-commit hooks
-    session.notify("initialize_commit_hooks")
+    session.notify('initialize_commit_hooks')
 
 
 @nox.session
 def lint(session: nox.Session):
     """Run linters."""
-    session.install("ruff")
-    session.run("ruff", "check", "--fix")
+    session.install('ruff')
+    session.run('ruff', 'check', '--fix')
 
 
 @nox.session
 def ruff_format(session: nox.Session):
     """Run formatters."""
-    session.install("ruff")
-    session.run("ruff", "format")
+    session.install('ruff')
+    session.run('ruff', 'format')
+
+
+@nox.session
+def observer(session: nox.Session):
+    """Run non-destructive checks."""
+    session.install('ruff', 'pytest')
+
+    # Check linting issues
+    session.run('ruff', '-s', 'check')
+
+    # Check tests
+    session.run('pytest', 'tests/unit', '--tb=no')
 
 
 @nox.session
@@ -107,44 +121,44 @@ def initialize_commit_hooks(session: nox.Session):
     # If not in a git repo, then pre-commit will fail. This should *not* be
     # considered an error, since the git repo should be set up by the person
     # running the script.
-    if not Path(".git").exists():
+    if not Path('.git').exists():
         session.log(
-            "Not in a git repository. Skipping pre-commit installation."
-            "If you meant to install pre-commit, please initialize "
-            "this repository (`git init`)."
+            'Not in a git repository. Skipping pre-commit installation.'
+            'If you meant to install pre-commit, please initialize '
+            'this repository (`git init`).'
         )
 
         return
 
-    session.install("pre-commit")
+    session.install('pre-commit')
 
     # May be external --- doesn't need to be installed locally.
     session.run(
-        "pre-commit",
-        "install",
-        "--install-hooks",
-        "--hook-type=pre-commit",
-        "--hook-type=commit-msg",
+        'pre-commit',
+        'install',
+        '--install-hooks',
+        '--hook-type=pre-commit',
+        '--hook-type=commit-msg',
         external=True,
     )
 
 
 # Testing
-@nox.session(python=["3.10", "3.11", "3.12"])
+@nox.session(python=['3.10', '3.11', '3.12'])
 def unit_tests(session: nox.Session):
     """Run unit tests."""
-    session.install("pytest")
-    session.run("pytest", "tests/unit", *session.posargs)
+    session.install('pytest')
+    session.run('pytest', 'tests/unit', *session.posargs)
 
 
-@nox.session(python=["3.10", "3.11", "3.12"])
+@nox.session(python=['3.10', '3.11', '3.12'])
 def integration_tests(session: nox.Session):
     """Run integration tests."""
-    session.install("pytest")
-    session.run("pytest", "tests/integration", *session.posargs)
+    session.install('pytest')
+    session.run('pytest', 'tests/integration', *session.posargs)
 
 
-@nox.session(python=["3.10", "3.11", "3.12"])
+@nox.session(python=['3.10', '3.11', '3.12'])
 def build_tests(session: nox.Session):
     """Run build tests."""
 
@@ -153,5 +167,5 @@ def build_tests(session: nox.Session):
 @nox.session
 def build(session: nox.Session):
     """Build the project."""
-    session.install("poetry")
-    session.run("poetry", "build", "--output=dist")
+    session.install('poetry')
+    session.run('poetry', 'build', '--output=dist')
